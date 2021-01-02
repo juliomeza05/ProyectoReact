6358aa28b5d7e4a9280d7922630419129cdb5689
julio meza <juliomeza05@hotmail.com>
2021-01-02 18:40:23 -0500

    Modificacion

--- index.js
+++ index.js
@@ -10,19 +10,12 @@ conectarDB();
 //habilitar cors
 app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 
 //habilitar express.json
 app.use(express.json({ extended : true }));
 
 //puerto de la APP
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 //importar rutas
 app.use('/api/usuarios',require('./Routes/usuarios'));
