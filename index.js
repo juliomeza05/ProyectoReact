e25bb66812f48c4a295418efc15166389e448525
julio meza <juliomeza05@hotmail.com>
2021-01-02 18:02:36 -0500

    cambios para la compilacion

--- index.js
+++ index.js
@@ -33,5 +33,5 @@ app.use('/api/Tareas',require('./Routes/Tareas'));
 
 //arrancar la APP
 app.listen(port,'0.0.0.0',() =>{
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`);   
+    console.log(`El servidor esta funcionando en el puerto ${port}`);   
 });
