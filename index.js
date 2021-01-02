bb991b4533fffe11725040438a40eee358a28d08
julio meza <juliomeza05@hotmail.com>
2021-01-02 17:52:55 -0500

    perrmitir los cors

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const app = express();
 conectarDB();
 //habilitar cors
 app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 //habilitar express.json
 app.use(express.json({ extended : true }));
 
